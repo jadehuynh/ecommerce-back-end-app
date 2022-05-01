dbfcd3a4aee6de2350e74e01086e57b12d125c36
jadehuynh <jadehuynh08@gmail.com>
2022-05-01 16:52:07 -0700

    adding more product/tag data

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -6,7 +6,23 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+  },
+    product_id: {
+      type: DataTypes.INTEGER,
+  },
+  tag_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'product_tag',
+      key: 'id',
+    },
+  },
   },
   {
     sequelize,
